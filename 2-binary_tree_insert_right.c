6821e7214889407310d4515a22d109fdd3853e69
Fabri <oviedofabrizzio1998@gmail.com>
2024-08-06 15:39:07 +0000

    task 2 finish

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -5,26 +5,26 @@
  * @value: The value of the new node
  *
  * Return: The new node
- *              If parent is NULL or occurs an error - NULL
+ *         If parent is NULL or occurs an error - NULL
  */
 
- binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
- {
-    binary_tree_t *new;
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
 
-    if (parent == NULL)
-            return (NULL);
+	if (parent == NULL)
+		return (NULL);
 
-    new = binary_tree_node(parent, value);
-    if (new == NULL)
-            return (NULL);
-    
-    if (parent->right != NULL)
-    {
-        new->right = parent->right;
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
+		return (NULL);
+
+	if (parent->right != NULL)
+	{
+		new->right = parent->right;
 		parent->right->parent = new;
-    }
+	}
 
-    parent->right = new;
-    return (new);
- }
+	parent->right = new;
+	return (new);
+}
