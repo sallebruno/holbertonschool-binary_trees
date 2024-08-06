efa079ac6510d775999036a552fbb3ce636e5a97
S4LL38RUN0 <9392@holbertonstudents.com>
2024-08-06 11:02:55 -0300

    0 comments

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,9 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_node - Creates a binary tree node
+* @parent - a parent to the node to be created
+* @value - value to be in the new node
+* Return: a pointer to the new node
+*               If an error occurrs - NULL
+*/
