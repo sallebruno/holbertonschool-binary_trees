59f6f0b2487f5d1feef13118d96b355f321b31e4
Fabri <oviedofabrizzio1998@gmail.com>
2024-08-06 18:16:04 +0000

    task 7

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - goes through a binary tree in-order
+ * @tree: pointer to tree
+ * @func: funcion to call each node
+ */
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree && func)
+	{
+		binary_tree_inorder(tree->left, func);
+		func(tree->n);
+		binary_tree_inorder(tree->right, func);
+	}
+}
