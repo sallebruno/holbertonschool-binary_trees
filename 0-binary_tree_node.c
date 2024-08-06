854fe42879a46d4d241d41d85496b510b0f36586
S4LL38RUN0 <9392@holbertonstudents.com>
2024-08-06 11:23:01 -0300

    update 0-binary_tree

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,10 +7,11 @@
 * Return: a pointer to the new node
 *               If an error occurrs - NULL
 */
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
     binary_tree_t *new;
-    
+
     new = malloc(sizeof(binary_tree_t));
     if (new == NULL)
         return (NULL);
