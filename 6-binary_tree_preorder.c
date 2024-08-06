d79a21360ca853bad55eff892ae212c25c0f8e8a
S4LL38RUN0 <9392@holbertonstudents.com>
2024-08-06 14:24:36 -0300

    comments and prototype task 6

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,5 +1,12 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_preorder - Analyses 
+* binary_tree_preorder - Analyses the binary tree using pre-order.
+* @tree: A pointer for the root node to use pre-order.
+* @func: A pointer to call for all nodes.
 */
+
+int binary_tree_is_root(const binary_tree_t *node);
+{
+	
+}
