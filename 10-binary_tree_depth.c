f7d53739311b586cf930806441c0c68adfd6b989
sallebruno <9392@holbertonstudents.com>
2024-08-06 22:33:39 +0000

    task 10 finished

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -6,10 +6,9 @@
  *
  * Return: The depth of the node if it exists, otherwise 0.
  */
-size_t calculate_node_depth(const binary_tree_t *node)
+size_t binary_tree_depth(const binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL)
-		return 0;
-	else
-		return 1 + calculate_node_depth(node->parent);
+		return (0);
+	return (1 + binary_tree_depth(node->parent));
 }
