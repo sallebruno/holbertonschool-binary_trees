b69e6b63ff6a8e6ade1454ab6723c494de81d0e3
Fabri <oviedofabrizzio1998@gmail.com>
2024-08-07 17:46:03 +0000

    task 16

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - checks if binary tree is perfect
+ * @tree: pointer to the root
+ *
+ * Return: 1 or 0
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+}
