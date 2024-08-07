41c6a1f52eaf62f2b77798ffae158686cdfbfabb
Fabri <oviedofabrizzio1998@gmail.com>
2024-08-07 16:51:36 +0000

    task 14 finish

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -17,7 +17,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		return ((l > r) ? l : r);
 	}
-	return (0);
+	return (-1);
 }
 
 /**
