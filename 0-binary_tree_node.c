5268575db60a4e6b749090686264e81d7f9c2ed6
sallebruno <9392@holbertonstudents.com>
2024-12-10 14:23:24 +0000

    betty 0 correction

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,13 +7,13 @@
  * @parent: Pointer to the parent node.
  * @value: Value for new node.
  * Return: New node if there's no error.
- * 		Otherwise - NULL
+ *		Otherwise - NULL
  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
-	
+
 	new = malloc(sizeof(binary_tree_t));
 	if (new == NULL)
 		return (NULL);
