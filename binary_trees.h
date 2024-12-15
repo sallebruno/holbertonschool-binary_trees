59244f861723967dcc3b16fd17671ad20cb9e319
sallebruno <9392@holbertonstudents.com>
2024-12-15 22:56:03 +0000

    task 9

--- binary_trees.h
+++ binary_trees.h
@@ -50,4 +50,4 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
-#endif /* _BINARY_TREES_H_ */
+#endif
