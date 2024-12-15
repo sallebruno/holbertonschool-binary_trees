43e7b424b47d7b718b6fe4f09aab798587ce4854
sallebruno <9392@holbertonstudents.com>
2024-12-15 00:05:04 +0000

    task 18

--- binary_trees.h
+++ binary_trees.h
@@ -34,6 +34,7 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
 int binary_tree_balance(const binary_tree_t *tree);
